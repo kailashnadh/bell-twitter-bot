15e1bdcc60d0d75af8eeab460c3827c39669f133
kailashnadh <46115792+kailashnadh@users.noreply.github.com>
2022-02-28 21:17:39 -0500

    added logs (#4)
    * added logs
    
    * added logs

--- src/main/java/com/bell/twitterbot/controller/TwitterNewsController.java
+++ src/main/java/com/bell/twitterbot/controller/TwitterNewsController.java
@@ -1,10 +1,10 @@
 package com.bell.twitterbot.controller;
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.social.ResourceNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,22 +24,26 @@ public class TwitterNewsController {
 
 	@Autowired
 	private NewsService newsService;
-	
-	private static final Logger logger = Logger.getLogger(TwitterNewsController.class);
+
+	private static final Logger logger = LoggerFactory.getLogger(TwitterNewsController.class);
 
 	@PostMapping("/news")
 	public ResponseEntity<PostNewsResponse> postNewsinTwitter(@RequestBody PostNewsRequest request) {
-		logger.info(String.format("postNewsinTwitter triggered with "
-				+ "topic {0}", request.getTopic()));
+		logger.info("postNewsinTwitter triggered with topic {}", request.getTopic());
 		if((null == request.getTopic()) || "".equals(request.getTopic())){
 			logger.error("Topic not provided");
-			throw new ResourceNotFoundException(request.getTopic(),"Topic not provided"); 
+			return(new ResponseEntity("topic_not_provided", HttpStatus.BAD_REQUEST));
 		}
 		String newsUrl = newsService.getNewsArticleURL(request.getTopic());
 		if(newsUrl.isEmpty()){
-			return(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+			return(new ResponseEntity("article_not_found", HttpStatus.NOT_FOUND));
 		}
-		twitterService.postTweetOnTopic(request.getTopic(), newsUrl);
-		return new ResponseEntity<>(new PostNewsResponse(newsUrl), HttpStatus.OK);
+		try {
+			twitterService.postTweetOnTopic(request.getTopic(), newsUrl);
+			return new ResponseEntity<>(new PostNewsResponse(newsUrl), HttpStatus.OK);
+		}
+		catch (Exception e){
+			return new ResponseEntity(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 }
--- src/main/java/com/bell/twitterbot/service/TwitterLibraryServiceImpl.java
+++ src/main/java/com/bell/twitterbot/service/TwitterLibraryServiceImpl.java
@@ -1,32 +1,29 @@
 package com.bell.twitterbot.service;
 
-import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Value;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Primary;
-import org.springframework.social.twitter.api.impl.TwitterTemplate;
 import org.springframework.stereotype.Component;
 import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
 
-import java.util.List;
 
-@Primary
 @Component
+@Primary
 public class TwitterLibraryServiceImpl implements TwitterService{
 
-    private static final Logger logger = Logger.getLogger(TwitterSpringServiceImpl.class);
+    private static final Logger logger = LoggerFactory.getLogger(TwitterLibraryServiceImpl.class);
 
     public void postTweetOnTopic(String topic, String url) {
         try {
             Twitter twitter = getTwitterInstance();
             String screenName = twitter.getScreenName();
+            logger.info("Screen name of twitter profile is {}", screenName);
             twitter.updateStatus(topic+" "+url);
-            logger.info(String.format("tweet completed with URL {0}", url));
         }
         catch (Exception e) {
-            logger.error(String.format("tweet failed in Exception with message "
-                    + " {0}", e.getMessage()));
-            System.out.println("Twitter Error in Exception"+e.getMessage());
+            logger.error("postTweetOnTopic failed - ", e.getMessage());
+            throw new RuntimeException(e.getMessage());
         }
     }
 

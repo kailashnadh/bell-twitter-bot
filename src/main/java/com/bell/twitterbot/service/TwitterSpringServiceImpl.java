19ec53aaac49c3b5a56df0c6ebf4f4f29dd92c17
kailashnadh <mkailashnadh@gmail.com>
2022-02-28 21:15:08 -0500

    added logs

--- src/main/java/com/bell/twitterbot/service/TwitterSpringServiceImpl.java
+++ src/main/java/com/bell/twitterbot/service/TwitterSpringServiceImpl.java
@@ -2,7 +2,6 @@ package com.bell.twitterbot.service;
 
 import java.util.List;
 
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.social.ApiException;
 import org.springframework.social.twitter.api.Tweet;
@@ -29,23 +28,14 @@ public class TwitterSpringServiceImpl implements TwitterService{
 	@Value("${twitter_access_token_secret}")
 	private String twitter_access_token_secret;
 	
-	private static final Logger logger = Logger.getLogger(TwitterSpringServiceImpl.class);
-	
 	public void postTweetOnTopic(String topic, String url) {
 		try {
 			Twitter twitter = getTwitterInstance();
-			if (twitter.isAuthorized()) {
-				logger.info(String.format("Authorized"));
-			} else {
-				logger.info(String.format("Not Authorized"));
-			}
 			String profileId = twitter.userOperations().getScreenName();
 			TweetData tweetData = new TweetData(topic+" "+url);
 			//tweetData.withMedia(new FileUrlResource(url));
 			List<Tweet> userTimeline = twitter.timelineOperations().getUserTimeline();
 			twitter.timelineOperations().updateStatus(tweetData);
-
-			logger.info(String.format("tweet completed with URL {0}", url));
 		} 
 //		catch (MalformedURLException e) {
 //			logger.error(String.format("tweet failed in MalformedURLException with message "
@@ -53,8 +43,6 @@ public class TwitterSpringServiceImpl implements TwitterService{
 //			System.out.println("Twitter Error in MalformedURLException"+e.getMessage());
 //		} 
 		catch (Exception e) {
-			logger.error(String.format("tweet failed in Exception with message "
-					+ " {0}", e.getMessage()));
 			System.out.println("Twitter Error in Exception"+e.getMessage());
 			throw new ApiException(topic,e.getMessage());
 		}
